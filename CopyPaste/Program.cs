98b096ecdc34d6e9761a7f30af63864935fdf64f
Shrimp <lcamara0916@gmail.com>
2022-08-23 14:51:54 +0000

    Add exercise to copy and paste files given 2 directories and if it can be overriden or not!

---
+++ CopyPaste/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program {
+    static void Main(string[] args)
+    {
+
+        bool shouldOverride;
+
+        string file = File.ReadAllText("./File.txt");
+
+        if(Boolean.TryParse(args[2], out shouldOverride)){
+            Console.WriteLine(shouldOverride);
+            File.Copy(args[0], args[1], shouldOverride);
+        }
+
+    }
+
+}
