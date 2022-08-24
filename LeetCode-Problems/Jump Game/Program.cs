d8c35f6c326e4ad1bb70bd5fde2777ff594fa16e
Shrimp <lcamara0916@gmail.com>
2022-08-24 15:06:54 +0000

    Add leet code problem - Jump Game

---
+++ LeetCode-Problems/Jump Game/Program.cs
@@ -0,0 +1,30 @@
+﻿//Exercise:
+//https://leetcode.com/problems/jump-game/
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        int[] nums = { 5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0 };
+        Console.WriteLine(CanJump(nums));
+
+    }
+
+    public static bool CanJump(int[] nums)
+    {
+
+        int possibleJump = nums.Length - 1;
+
+        for (int i = nums.Length - 2; i >= 0; i--)
+        {
+            if(i + nums[i] >= possibleJump){
+                possibleJump = i;
+            }
+        }
+
+        return possibleJump == 0;
+
+    }
+}
