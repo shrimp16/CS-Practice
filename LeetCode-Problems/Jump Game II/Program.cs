fadc614ae91cb68686430ecea741df73ce98b8a9
Shrimp <lcamara0916@gmail.com>
2022-08-24 16:58:17 +0000

    Add leet code problem - Jump Game II

---
+++ LeetCode-Problems/Jump Game II/Program.cs
@@ -0,0 +1,37 @@
+﻿//Problem: 
+//https://leetcode.com/problems/jump-game-ii/
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        int[] numbers = { 2, 3, 0, 1, 4 };
+
+        Jump(numbers);
+    }
+
+    public static int Jump(int[] nums)
+    {
+
+        int max = 0;
+        int currentCover = 0;
+        int moves = 0;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            max = Math.Max(max, i + nums[i]);
+
+            if (i == currentCover)
+            {
+                moves++;
+                currentCover = max;
+            }
+
+        }
+
+        return moves;
+
+    }
+
+}
