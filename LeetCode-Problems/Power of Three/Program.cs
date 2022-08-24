846d056069363300ac2ac93923b23a33b2f157f2
Shrimp <lcamara0916@gmail.com>
2022-08-24 15:31:30 +0000

    Add leet code problem - Power of Three

---
+++ LeetCode-Problems/Power of Three/Program.cs
@@ -0,0 +1,18 @@
+﻿//Problem: 
+//https://leetcode.com/problems/power-of-three/
+
+class Program {
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsPowerOfThree(Int32.Parse(args[0])));
+    }
+
+    static bool IsPowerOfThree(int n ){
+        double number = n;
+        while(number > 1){
+            number = (double) number / 3;
+        }
+        return number == 1;
+    }
+}
