ea80ec6a7d3fde674988f9a3946cefc3fed3d230
Shrimp <lcamara0916@gmail.com>
2022-08-24 17:26:17 +0000

    Add leet code problem - Single Number

---
+++ LeetCode-Problems/Single Number/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program {
+    static void Main(string[] args)
+    {
+        int ans = 0;
+        int[] nums = {2, 4, 2, 1, 4};
+
+        for(int i = 0; i < nums.Length; i++){
+            ans ^= nums[i];
+        }
+
+        Console.Write(ans);
+    }
+}
